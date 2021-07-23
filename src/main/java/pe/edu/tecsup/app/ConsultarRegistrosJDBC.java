755a8889f92d088e028110ca758b89b78ecf828e
Jaime <jgomezm@tecsup.edu.pe>
2021-07-22 21:08:47 -0500

    Update

--- src/main/java/pe/edu/tecsup/app/ConsultarRegistrosJDBC.java
+++ src/main/java/pe/edu/tecsup/app/ConsultarRegistrosJDBC.java
@@ -7,26 +7,37 @@ import java.sql.ResultSet;
 
 public class ConsultarRegistrosJDBC {
 
+	public static String URL = "jdbc:mysql://localhost/almacen?useSSL=false";
+	public static String USERNAME = "root";
+	public static String PASSWORD = "";
+	
 	public static void main(String[] args) {
+		
 		try {
 			// Cargar el driver
 			Class.forName("com.mysql.jdbc.Driver");
+			
 			// Conectarme a la BD
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/almacen?useSSL=false", "root", "");
+			Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
 			// Preparar la sentencia SQL
 			String sql = "select * from categorias";
 			PreparedStatement stmt = con.prepareStatement(sql);
 			ResultSet rs = stmt.executeQuery();
+			
 			while (rs.next()) {
 				int id = rs.getInt("id");
 				String nombre = rs.getString("nombre");
 				System.out.printf("id = %d , nombre=%s \n", id, nombre);
 			}
+			
 			rs.close();
 			stmt.close();
 			con.close();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
+			//System.err.println(e.getMessage());
 		}
 	}
 
