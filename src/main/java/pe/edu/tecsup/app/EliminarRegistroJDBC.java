25c99e1f9919f09475be0592216af0191b760bf6
Jaime <you@example.com>
2021-11-02 20:09:44 -0500

    Update

--- src/main/java/pe/edu/tecsup/app/EliminarRegistroJDBC.java
+++ src/main/java/pe/edu/tecsup/app/EliminarRegistroJDBC.java
@@ -25,10 +25,13 @@ public class EliminarRegistroJDBC {
 
 			// Preparar la sentencia SQL
 			String sql = "DELETE FROM categorias WHERE id = ?"; 
+			
+			//
 			PreparedStatement stmt = con.prepareStatement(sql);
 			
 			// Configura el PK del registro a eliminar
-			stmt.setInt(1, 4); // EL registro con ID igual a 4 existe
+			int id = 7;
+			stmt.setInt(1, id); // EL registro con ID igual a 7 existe
 			
 			// Ejecutar la insercion
 			int estado = stmt.executeUpdate(); 
@@ -36,6 +39,8 @@ public class EliminarRegistroJDBC {
 			if (estado != 1)
 				throw new SQLException("No se pudo eliminar");
 			
+			System.out.printf("Se elimino la categoria con id = %d \n", id);
+
 			// Cerrar conexiones
 			stmt.close();
 			con.close();
